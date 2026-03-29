44247dd8ef64da9772e909fc809405f79000c8f5
Kunavoot <kunavoot.k@gmail.com>
2026-03-29 14:10:50 +0700

    update CA Cert

--- config/db.js
+++ config/db.js
@@ -1,11 +1,19 @@
 const mysql = require('mysql2');
+const fs = require('fs');
+const path = require('path');
 require('dotenv').config();
 
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    ssl: process.env.CA_PATH ? {
+        minVersion: 'TLSv1.2',
+        ca: fs.readFileSync(path.join(__dirname, '..', process.env.CA_PATH)),
+        rejectUnauthorized: true
+    } : undefined,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
